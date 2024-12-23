c92cd6fbf81d2aac43e0dee6e2e4a54c42cd1c86
AkashNaik11 <naikaaakash7735@gmail.com>
2024-12-23 20:03:54 +0530

    Made a currency converter

---
+++ src/hooks/useCurrencyinfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+
+function useCurrencyinfo(currency){
+    const [data, setData]= useState({})
+    useEffect(()=>{
+        fetch(`https://currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+        console.log(data);
+
+    },[currency])
+    console.log(data)
+    return data
+}
+
+export default useCurrencyinfo;
